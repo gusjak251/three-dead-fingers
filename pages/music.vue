03c7ff5660cb3ae785b4da96695c3ee3e9a606bb
Gustav Jakobsson <gustav.jakobsson@abbgymnasiet.se>
2023-10-03 12:53:07 +0200

    Create music page

---
+++ pages/music.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-slate-950 min-h-screen">
+    <Navbar />
+    <Header />
+    <Press-Photos />
+  </div>
+</template>
+  
+<script>
+export default {
+  name: 'MusicPage'
+}
+</script>
+  
