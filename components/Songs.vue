a7725eae6578a70e96f69d3ca956468d181da8e4
Gustav Jakobsson <gustav.jakobsson@abbgymnasiet.se>
2023-10-03 13:09:12 +0200

    Create component for listing unreleased songs

---
+++ components/Songs.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex flex-column justify-around gap-y-2 w-full">
+        <div v-for="song in songs" :key="song.title" class="text-white text-center">
+            <Title>{{ song.title }}</Title>
+            <p>{{ song.description }}</p>
+            <audio></audio>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'Songs',
+    data() {
+        return {
+            songs: [
+                {
+                    'link': 'images/fiskis.jpg',
+                    'title': 'Coward in blue',
+                    'description': 'Lallish song',
+                },
+                {
+                    'link': 'images/fiskis.jpg',
+                    'title': 'Five O Zero',
+                    'description': 'Create a gap in time!'
+                },
+            ]
+        }
+    }
+}
+</script>
+  
