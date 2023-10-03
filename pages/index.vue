716f2c6026ac8d49d7c65468d1212cf9eb7d9470
Gustav Jakobsson <gustav.jakobsson@abbgymnasiet.se>
2023-10-03 12:58:01 +0200

    Change font family

--- pages/index.vue
+++ pages/index.vue
@@ -12,3 +12,11 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Gabarito&display=swap');
+
+* {
+  font-family: 'Gabarito', cursive;
+}
+</style>
