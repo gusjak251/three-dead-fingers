05c1470903822e5e5ad959f8c52e14c8b96d82a0
Gustav Jakobsson <gustav.jakobsson@abbgymnasiet.se>
2023-10-03 13:15:12 +0200

    Create page title component

---
+++ components/PageTitle.vue
@@ -0,0 +1,13 @@
+<!-- Please remove this file from your project -->
+<template>
+    <h1 class="text-3xl text-white text-center tracking-wider p-2 my-4">
+        <slot></slot>
+    </h1>
+</template>
+  
+<script>
+export default {
+    name: 'PageTitle'
+}
+</script>
+  
