6ccebf28f979e6e2b2b6b44c5e4b3ae99602a761
gusjak251 <gustavjakobsson3@gmail.com>
2023-10-05 10:26:53 +0200

    Add gallery scroll functionality

--- components/Press-Photos.vue
+++ components/Press-Photos.vue
@@ -1,17 +1,18 @@
 <!-- Please remove this file from your project -->
 <template>
     <div>
-        <div class="flex flex-row w-1/3 mx-auto">
-            <button>
+        <div class="flex flex-row mx-auto w-1/2">
+            <button @click="scroll(-1)">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray"
                     class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                 </svg>
             </button>
-            <div v-for="image in images" :key="image.link" class="p-4">
-                <img :src="image.link" class="h-36 w-48 cursor-pointer hover:opacity-75" @click="selectImage(image)" />
+            <div v-for="image in filterImages()" :key="image.link" class="p-4">
+                <img :src="image.link" class="h-48 object-cover cursor-pointer hover:opacity-75 transition-all"
+                    @click="selectImage(image)" />
             </div>
-            <button>
+            <button @click="scroll(1)">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray"
                     class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
@@ -21,7 +22,8 @@
         <div v-if="showModal"
             class="fixed flex justify-center items-center w-screen h-screen bg-black bg-opacity-50 left-0 top-0 z-50">
             <p class="text-center fixed top-10 mx-auto text-white text-lg">{{ selectedImage.text }}</p>
-            <img v-click-outside="closeModal" :src="selectedImage.link" class="sm:w-3/4 md:w-1/2 text-center mx-auto" />
+            <img v-click-outside="closeModal" :src="selectedImage.link"
+                class="sm:w-3/4 md:w-1/2 h-3/4 text-center mx-auto" />
         </div>
     </div>
 </template>
@@ -38,6 +40,36 @@ export default {
             this.selectedImage = image;
             this.showModal = true;
         },
+        scroll(direction) {
+            if (this.images.length <= this.imageCount) {
+                return;
+            }
+            const step = this.position + direction;
+            if (step > this.images.length - 1) {
+                this.position = 0;
+            } else if (step < 0) {
+                this.position = this.images.length - 1;
+            } else {
+                this.position = step;
+            }
+
+            console.log(this.position);
+        },
+        filterImages() {
+            if (this.images.length <= this.imageCount) {
+                return this.images;
+            }
+            if (this.position + this.imageCount - 1 > this.images.length - 1) {
+                let merged_images = this.images.slice(this.position, this.images.length);
+                const diff = this.imageCount - merged_images.length;
+                this.images.slice(0, diff).forEach(image => {
+                    merged_images.push(image);
+                });
+                console.log(merged_images);
+                return merged_images;
+            }
+            return this.images.slice(this.position, this.position + this.imageCount);
+        },
         closeModal() {
             this.showModal = false;
         }
@@ -52,13 +84,23 @@ export default {
                 {
                     'link': 'images/logo.png',
                     'text': 'Fiskis playing guitar'
-                }
+                },
+                {
+                    'link': 'images/header-image.JPG',
+                    'text': 'Fiskis playing guitar'
+                },
+                {
+                    'link': 'images/IMG_0635.JPG',
+                    'text': 'Fiskis playing guitar'
+                },
             ],
             selectedImage: {
                 'link': '',
                 'text': 'Image not found'
             },
             showModal: false,
+            position: 0,
+            imageCount: 3,
         }
     }
 }
