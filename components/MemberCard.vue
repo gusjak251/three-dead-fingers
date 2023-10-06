971f20259d1277ca8d5d850e6f935674f4f4a8e6
gusjak251 <gustavjakobsson3@gmail.com>
2023-10-06 18:07:24 +0200

    Add member card component

---
+++ components/MemberCard.vue
@@ -0,0 +1,20 @@
+<!-- Please remove this file from your project -->
+<template>
+    <div class="text-center w-48">
+        <Title>{{ name }}</Title>
+        <p class="text-gray-700 text-sm">{{ role }}</p>
+        <img class="h-48 object-cover" :src="img" />
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        name: String,
+        role: String,
+        img: String,
+    },
+    name: 'MemberCard'
+}
+</script>
+  
