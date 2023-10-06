dd6628b5b7529302d0c3a18aa30484cb3b1c14d5
gusjak251 <gustavjakobsson3@gmail.com>
2023-10-06 14:40:53 +0200

    Make bio responsive

--- components/Bio.vue
+++ components/Bio.vue
@@ -1,9 +1,9 @@
 <!-- Please remove this file from your project -->
 <template>
-    <div id="main" class="flex flex-row justify-around w-full mx-auto">
-        <img src="/images/fiskis.JPG" class="p-10 w-1/2 h-96 max-h-80 object-cover" />
-        <div class="p-10 mt-8 w-1/2">
-            <Title class="mb-2">Bio</Title>
+    <div id="main" class="flex flex-col md:flex-row justify-around w-full mx-auto">
+        <img src="/images/fiskis.JPG" class="p-10 w-full md:w-1/2 h-96 object-cover object-top" />
+        <div class="p-10 mt-0 md:mt-8 w-full md:w-1/2 text-left mx-auto md:mx-0">
+            <Title class="mb-2">Biography</Title>
             <p class="text-white">
                 Three Dead Fingers, the sound of a new generation. A band from Sweden that started out in 2015 as 10-11 year
                 olds trying to play heavy music has now turned into one of the most promising acts on the map today.
